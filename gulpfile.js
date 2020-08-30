2f485da15a34e961c89e92e5cd1533ef720eff2d
John Svensson <john@kodamera.se>
2020-08-30 16:41:50 +0200

    add purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const purgecss = require('gulp-purgecss');
 
 sass.compiler = require('node-sass');
 
 function build() {
   return gulp.src('./css/main.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(purgecss({
+      content: ['./templates/**/*.html.twig']
+    }))
     .pipe(gulp.dest('./css/dist'));
 }
 
